df74393047a17ef2b87b1e4995a33738c2759f77
Darieeld <carlos.dariel.arrieta@gmail.com>
2026-05-04 22:38:58 -0500

    Kael

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server', // <-- Asegúrate de que diga 'server'
+  output: 'server',
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
+  // Forzamos a Astro a usar la ruta que Cloudflare te obliga a poner
+  outDir: './dist', 
 });
