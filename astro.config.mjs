1e4bf7069da29eecd3f8968ae93ccafbd00ae46c
Darieeld <carlos.dariel.arrieta@gmail.com>
2026-05-05 09:11:43 -0500

    Cambiando adaptador de Cloudflare a Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare'; // <--- Esto debe estar instalado
 
+import vercel from '@astrojs/vercel';
+
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'server', // O 'hybrid' si solo algunas páginas son dinámicas
-  adapter: cloudflare(),
-}); 
+  adapter: netlify(),
+});
