f667b42286b61545235a3c1760652a26ec51ac47
Darieeld <carlos.dariel.arrieta@gmail.com>
2026-05-05 09:43:04 -0500

    Versión final purificada: Solo Netlify y precios en float8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare'; // <--- Esto debe estar instalado
-
-import vercel from '@astrojs/vercel';
-
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server', // O 'hybrid' si solo algunas páginas son dinámicas
+  output: 'server', 
   adapter: netlify(),
 });
